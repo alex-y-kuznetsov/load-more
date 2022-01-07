a8cd811a45ac2967949d67f090a783da773f805e
Alex <alex.y.kuznetsov@gmail.com>
2022-01-07 12:43:37 +0500

    setup

---
+++ src/components/LoadMore.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>Load More page</div>
+</template>
+
+<script>
+export default {
+  name: 'LoadMore',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped lang="less">
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <LoadMore />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import LoadMore from '@/components/LoadMore.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    LoadMore
   }
 }
 </script>
